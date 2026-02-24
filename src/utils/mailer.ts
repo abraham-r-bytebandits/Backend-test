27c3a1fa276c002ceb7b3ace485457a7b36ca4b9
abraham-r-bytebandits <abraham.r@bytebandits.in>
2026-02-24 11:16:10 +0530

    add the resend mail

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,22 +1,17 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendMail = async (
   to: string,
   subject: string,
   html: string
 ) => {
-  await transporter.sendMail({
-    from: `"Ecommerce App" <${process.env.EMAIL_USER}>`,
+  await resend.emails.send({
+    from: process.env.EMAIL_FROM ?? "Ecommerce App <onboarding@resend.dev>",
     to,
     subject,
     html,
   });
 };
+
